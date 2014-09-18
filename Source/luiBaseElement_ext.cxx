14d4491893f62ffe8b8ee726ec7e391733ab991b
tobspr <tobias.springer1@googlemail.com>
2014-09-18 17:15:05 +0200

    Updated the luiBaseElement __setattr_

--- Source/luiBaseElement_ext.cxx
+++ Source/luiBaseElement_ext.cxx
@@ -8,16 +8,35 @@
 #ifdef HAVE_PYTHON
 
 int Extension<LUIBaseElement>::__setattr__(PyObject *self, PyObject *name, PyObject *value) {
-  cout << "__setattr__ (LUIBaseElement) called for '" << name << "'" << endl;
+
+  // Try to find a method called "set_<name>"
+  PyObject *str = PyObject_Str(name);
+  string name_as_str;
+  if (str != NULL) {
+    name_as_str = PyString_AS_STRING(str);
+  }
+  cout << "__setattr__ (LUIBaseElement) called for '" << name_as_str << "'" << endl;
+
+  if (str != NULL) {
+    Py_DECREF(str);
+  }
 
   PyObject *setter_name = PyString_FromString("set_");
   PyString_Concat(&setter_name, name);
 
   PyObject *result = PyObject_CallMethodObjArgs(self, setter_name, value, NULL);
 
+  // Did not find any method, that means we save the value in the class dict
   if (result == NULL) {
     Py_DECREF(setter_name);
-    return -1;
+
+    // Write to class dictionary
+    cout << "Could not find element, saving in class dict" << endl; 
+    PyObject* __dict__ = PyObject_GenericGetAttr(self, (char *)string("__dict__").c_str()); 
+    PyDict_SetItem(__dict__, name, value); 
+
+    Py_DECREF(__dict__);
+    return 0;
   }
 
   Py_DECREF(setter_name);
@@ -26,21 +45,32 @@ int Extension<LUIBaseElement>::__setattr__(PyObject *self, PyObject *name, PyObj
 }
 
 PyObject *Extension<LUIBaseElement>::__getattr__(PyObject *self, PyObject *name) {
-  cout << "__getattr__ (LUIBaseElement) called for '" << name << "'" << endl;
 
+  // Try to find a method called "get_<name>"
+  PyObject *str = PyObject_Str(name);
+  string name_as_str;
+  if (str != NULL) {
+    name_as_str = PyString_AS_STRING(str);
+  }
+  cout << "__getattr__ (LUIBaseElement) called for '" << name_as_str << "'" << endl;
+
+  if (str != NULL) {
+    Py_DECREF(str);
+  }
   PyObject *getter_name = PyString_FromString("get_");
   PyString_Concat(&getter_name, name);
 
   PyObject *getter = PyObject_GenericGetAttr(self, getter_name);
 
+  // No method found, just return, interrogate will fix this for us
   if (getter == NULL) {
     Py_DECREF(getter_name);
     return NULL;
   }
 
+  // Calls the "get_<name>" and returns the result
   PyObject *return_value = PyObject_CallObject(getter, NULL);
   Py_DECREF(getter_name);
-  // Py_DECREF(getter);
   return return_value;
 }
 
