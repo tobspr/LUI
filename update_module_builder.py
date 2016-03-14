3bef1825a3d0cf0ea8cbc7b3855f6f398be466e0
tobspr <tobias.springer1@googlemail.com>
2016-03-14 17:48:05 +0100

    Fix typo

--- update_module_builder.py
+++ update_module_builder.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
         "Source/config_module.h Source/ExampleClass.cpp Source/ExampleClass.h Source/ExampleClass.I").split()
     curr_dir = os.path.dirname(os.path.realpath(__file__)); os.chdir(curr_dir);
     download_submodule("tobspr", "P3DModuleBuilder", curr_dir, ignore)
-    with open("Scripts/__init__.py", "w") as handle: pass
+    with open("scripts/__init__.py", "w") as handle: pass
     try: os.remove(".gitignore")
     except: pass
     os.rename("prefab.gitignore", ".gitignore")
